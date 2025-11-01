59c4cff4232eee37b8e4a28760e22460e33a5bb6
hello3world <e.pavlovich29@gmail.com>
2025-11-01 11:11:18 +0300

    update home page

---
+++ pages/home_page.py
@@ -0,0 +1,25 @@
+from pages.base_page import BasePage
+from playwright.sync_api import expect
+
+class HomePage(BasePage):
+    def __init__(self, page):
+        super().__init__(page)
+        self.page = page
+
+    def page_verify(self):
+        expect(self.page.get_by_alt_text("Selenium Online Training")).to_be_visible(timeout=10000)
+    
+    def click_elements_button(self):
+        self.page.get_by_text("Elements").click()
+
+    def click_forms_button(self):
+        self.page.get_by_text("Forms").click()
+
+    def click_alerts_button(self):
+        self.page.get_by_text("Alerts, Frame & Windows").click()
+
+    def click_widgets_button(self):
+        self.page.get_by_text("Widgets").click()
+
+    def click_interactions_button(self):
+        self.page.get_by_text("Interactions").click()
